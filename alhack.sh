8441a2ed05e0dfb5366e294f263e63b8f2ddac18
Adoumsoukaya <126183289+Adoumsoukaya@users.noreply.github.com>
2024-12-14 00:18:24 +0100

    Update Brut_hack.sh

--- alhack.sh
+++ alhack.sh
@@ -1,76 +1,77 @@
 clear
 mkdir Tools
-clear 
+clear
 echo -e '\033[31;40;1m 
-  █████╗ ██╗             ████████╗ ██████╗  ██████╗ ██╗
- ██╔══██╗██║             ╚══██╔══╝██╔═══██╗██╔═══██╗██║
- ███████║██║     ███████╗   ██║   ██║   ██║██║   ██║██║
- ██╔══██║██║     ╚══════╝   ██║   ██║   ██║██║   ██║██║
- ██║  ██║███████╗           ██║   ╚██████╔╝╚██████╔╝███████╗
- ╚═╝  ╚═╝╚══════╝           ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝ v3
-  Coded by 4lbH4cker
-  github: https://github.com/4lbH4cker
-\033[33;4mVersion:\033[0m 3            \033[33;4mCTRL+C:\033[0m exit          \033[33;4mAuthor:\033[0m 4lbH4cker
 
-\e[37m[1]\e[36m Kërkesat & Përditësimi        \e[37m[2]\e[36m Phishing Tool				
-\e[37m[3]\e[36m WebCam Hack                   \e[37m[4]\e[36m User Find					
-\e[37m[5]\e[36m Gmail Bomber		  \e[37m[6]\e[36m DDOS Attack			
-\e[37m[7]\e[36m Si të përdorni ?	          \e[37m[8]\e[36m Çinstaloni programet e shkarkuara		
-\e[37m[9]\e[36m Ip Info	                  \e[37m[10]\e[36m dorks-eye
+ ████████╗ ██████╗  ██████╗ ██╗     ███████╗
+╚═ ═██╔══╝██╔═══██╗██╔═══██╗██║     ██╔════╝
+    ██║   ██║   ██║██║   ██║██║       █████╗  
+    ██║   ██║   ██║██║   ██║██║          ██
+    ██║   ╚██████╔╝╚██████╔╝███████╗███████╗
+    ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝v3
+ 
+ Coded by Brut_hack 
+  github: https://github.com/Adoumsoukaya/
+\033[33;4mVersion:\033[0m 3            \033[33;4mCTRL+C:\033[0m exit          \033[33;4mAuthor:\033[0m Brut_hack 
+
+\e[37m[1]\e[36m Exigences & mise à jour        \e[37m[2]\e[36m Phishing Tool                            
+\e[37m[3]\e[36m WebCam Hack                   \e[37m[4]\e[36m User Find                                 
+\e[37m[5]\e[36m Gmail Bomber              \e[37m[6]\e[36m DDOS Attack                   
+\e[37m[7]\e[36m Comment utiliser ?                \e[37m[8]\e[36m Désinstallez les programmes téléchargés               
+\e[37m[9]\e[36m Ip Info                   \e[37m[10]\e[36m dorks-eye
 \e[37m[11]\e[36m ghost_eye                    \e[37m[12]\e[36m RED_HAWK
 \e[37m[13]\e[36m VirusCrafter                 \e[37m[14]\e[36m Info-Site
-\e[37m[15]\e[36m BadMod	                  \e[37m[16]\e[36m Facebash
+\e[37m[15]\e[36m BadMod                   \e[37m[16]\e[36m Facebash
 \e[37m[17]\e[36m DARKARMY                     \e[37m[18]\e[36m N-ANOM
 '
 
 
-#Zgjedhja e Opsionit
+#Sélection des options
 
-
-read -p "Numri i transaksionit: " islem
+read -p "Numéro de transaction : " islem
 if [[ $islem == 1 || $islem == 01 ]]; then
 clear
 
-echo -e "\033[47;31;5m Po instalon përditësimin dhe kërkesat...\033[0m"
+echo -e "\033[47;31;5m Installation de la mise à jour et des exigences...\033[0m"
 sleep 5
-pkg install git -y
-pkg install python python2 -y
-pkg install pip pip2 -y
-pkg install curl -y
-apt update
-apt upgrade -y
+sudo apt install git -y
+sudo apt install python python3 -y
+sudo apt install pip3
+sudo pip3 install curl -y
+sudo apt update
+sudo apt upgrade -y
 clear
-echo -e "\033[47;3;35m Përditësimi i plotë...\033[0m"
+echo -e "\033[47;3;35m Mise à jour complète...\033[0m"
 sleep 3
 bash tga.sh
 
 elif [[ $islem == 2 || $islem == 02 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Téléchargement...\033[0m"
 sleep 3
 cd Tools
-git clone https://github.com/htr-tech/zphisher
+git clone https://github.com/htr-tech/zphisher.git
 cd zphisher
 bash zphisher.sh
 
 elif [[ $islem == 3 || $islem == 03 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Téléchargement...\033[0m"
 sleep 3
 cd Tools
-git clone https://github.com/techchipnet/CamPhish
+git clone https://github.com/techchipnet/CamPhish.git
 cd CamPhish
 bash camphish.sh
 
 elif [[ $islem == 4 || $islem == 04 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Téléchargement...\033[0m"
 sleep 3
 cd Tools
-git clone https://github.com/xHak9x/finduser
+git clone https://github.com/xHak9x/finduser.git
 cd finduser
 bash finduser.sh
-       
+
 elif [[ $islem == 5 || $islem == 05 ]]; then
 clear
 echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
@@ -78,15 +79,15 @@ sleep 3
 cd Tools
 git clone https://github.com/palahsu/MBomb.git
 cd MBomb
-python MBomb.py
-
+#python3 MBomb.py
+python3 'MBomb v2.py'
 elif [[ $islem == 6 || $islem == 06 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Téléchargement...\033[0m"
 sleep 3
 cd Tools
 pip3 install requests pysocks
-git clone https://github.com/Leeon123/CC-attack
+git clone https://github.com/Leeon123/CC-attack.git
 cd CC-attack
 python3 cc.py
 
@@ -95,12 +96,12 @@ clear
 xdg-open https://pasteio.com/xuCvIkXdNRIB
 bash tga.sh
 
- 
+
 elif [[ $islem == 8 ]]; then
         clear
 
-echo -e "\033[47;3;35m PO HIQEN PROGRAMET E SHKARKUARA...\033[0m"
-sleep 3 
+echo -e "\033[47;3;35m SUPPRESSION DES PROGRAMMES TÉLÉCHARGÉS...\033[0m"
+sleep 3
 rm -rf Tools
 
 bash tga.sh
@@ -110,118 +111,121 @@ clear
 echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
 sleep 3
 cd Tools
-git clone https://github.com/ahgaurel1/ipinfo
+git clone https://github.com/ahgaurel1/ipinfo.git
 cd ipinfo
 bash install.sh
 python3 ipinfo.py
 
 elif [[ $islem == 10 || $islem == 010 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Téléchargement...\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/BullsEye0/dorks-eye.git
 cd dorks-eye
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 python3 dorks-eye.py
 
 elif [[ $islem == 11 || $islem == 011 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Téléchargement...\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/BullsEye0/ghost_eye.git
 cd ghost_eye
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 python3 ghost_eye.py
 
 elif [[ $islem == 12 || $islem == 012 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Téléchargement...\033[0m"
 sleep 3
 cd Tools
-git clone https://github.com/Tuhinshubhra/RED_HAWK
+git clone https://github.com/Tuhinshubhra/RED_HAWK.git
 cd RED_HAWK
 php rhawk.php
 
 elif [[ $islem == 13 || $islem == 013 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Téléchargement...\033[0m"
 sleep 3
 cd Tools
-git clone https://github.com/Devil-Tigers/TigerVirus
-apt update
-apt upgrade -y
-pkg install git -y
+git clone https://github.com/Devil-Tigers/TigerVirus.git
+sudo apt update
+sudo apt upgrade -y
+sudo apt install git -y
 cd TigerVirus
 bash TigerVirus.sh
 
 elif [[ $islem == 14 || $islem == 014 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Téléchargement...\033[0m"
 sleep 3
 cd Tools
-pkg install curl -y
-upgrade -y
-pkg install git -y
+sudo apt install curl -y
+sudo apt update
+sudo apt upgrade -y
+sudo apt install git -y
 git clone https://github.com/king-hacking/info-site.git
 cd info-site
 bash info.sh
 
 elif [[ $islem == 15 || $islem == 015 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Téléchargement...\033[0m"
 sleep 3
 cd Tools
-sudo apt-get update
-sudo apt-get install php
-sudo apt-get install php-curl
+sudo apt update
+sudo apt install php
+sudo apt install php-curl
 git clone https://github.com/MrSqar-Ye/BadMod.git
 cd BadMod
-chmod u+x INSTALL
-chmod u+x BadMod.php
+sudo chmod u+x INSTALL
+sudo chmod u+x BadMod.php
 sudo php BadMod.php
 
 elif [[ $islem == 16 || $islem == 016 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Téléchargement...\033[0m"
 sleep 3
 cd Tools
-git clone https://github.com/fu8uk1/facebash
+git clone https://github.com/fu8uk1/facebash.git
 cd facebash
-chmod +x facebash.sh
+sudo chmod +x facebash.sh
 service tor start
 sudo ./facebash.sh
 
 elif [[ $islem == 17 || $islem == 017 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Téléchargement...\033[0m"
 sleep 3
 cd Tools
-pkg install git
-pkg install python2
-git clone https://github.com/D4RK-4RMY/DARKARMY
+apt install git
+apt install python3
+git clone https://github.com/D4RK-4RMY/DARKARMY.git
 cd DARKARMY
-chmod +x darkarmy.py
-python2 darkarmy.py
+sudo chmod +x darkarmy.py
+python3 darkarmy.py
 
-elif [[ $islem == 18 || $islem == 018 ]]; then
+elif [[ $islem == 17 || $islem == 018 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Téléchargement...\033[0m"
 sleep 3
 cd Tools
-apt update && apt upgrade 
-pkg install git -y
-pkg install python && pkg install python3 -y
-git clone https://github.com/Nabil-Official/N-ANOM
+apt update && apt upgrade
+apt install git -y
+apt install python && pkg install python3 -y
+git clone https://github.com/Nabil-Official/N-ANOM.git
 pip3 install requests
-cd N-ANOM 
+cd N-ANOM
 python3 n-anom.py
 
-else   
-	clear
-        echo -e '\033[36;40;1m Keni futur kodin e gabuar'	
-	sleep 1
-	clear 
-	bash tga.sh
+else
+        clear
+        echo -e '\033[36;40;1m Vous avez entré le mauvais code' 
+        sleep 1
+        clear
+        bash tga.sh
 fi
+
+                                              
