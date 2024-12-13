f298c94e35c4e21e9015417af49fd1c77a6000da
Adoumsoukaya <126183289+Adoumsoukaya@users.noreply.github.com>
2024-12-14 00:24:55 +0100

    Update Brut_hack.sh

--- Brut_hack.sh
+++ Brut_hack.sh
@@ -15,14 +15,14 @@ echo -e '\033[31;40;1m
 \033[33;4mVersion:\033[0m 3            \033[33;4mCTRL+C:\033[0m exit          \033[33;4mAuthor:\033[0m Brut_hack 
 
 \e[37m[1]\e[36m Exigences & mise à jour        \e[37m[2]\e[36m Phishing Tool                            
-\e[37m[3]\e[36m WebCam Hack                   \e[37m[4]\e[36m User Find                                 
-\e[37m[5]\e[36m Gmail Bomber              \e[37m[6]\e[36m DDOS Attack                   
-\e[37m[7]\e[36m Comment utiliser ?                \e[37m[8]\e[36m Désinstallez les programmes téléchargés               
-\e[37m[9]\e[36m Ip Info                   \e[37m[10]\e[36m dorks-eye
-\e[37m[11]\e[36m ghost_eye                    \e[37m[12]\e[36m RED_HAWK
-\e[37m[13]\e[36m VirusCrafter                 \e[37m[14]\e[36m Info-Site
-\e[37m[15]\e[36m BadMod                   \e[37m[16]\e[36m Facebash
-\e[37m[17]\e[36m DARKARMY                     \e[37m[18]\e[36m N-ANOM
+\e[37m[3]\e[36m WebCam Hack                    \e[37m[4]\e[36m User Find                                 
+\e[37m[5]\e[36m Gmail Bomber                   \e[37m[6]\e[36m DDOS Attack                   
+\e[37m[7]\e[36m Comment utiliser ?             \e[37m[8]\e[36m Désinstallez les programmes téléchargés               
+\e[37m[9]\e[36m Ip Info                        \e[37m[10]\e[36m dorks-eye
+\e[37m[11]\e[36m ghost_eye                     \e[37m[12]\e[36m RED_HAWK
+\e[37m[13]\e[36m VirusCrafter                  \e[37m[14]\e[36m Info-Site
+\e[37m[15]\e[36m BadMod                        \e[37m[16]\e[36m Facebash
+\e[37m[17]\e[36m DARKARMY                      \e[37m[18]\e[36m N-ANOM
 '
 
 
